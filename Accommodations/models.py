2bd96cbe1e594680299094ae44fa9876ea0d9347
Seunghyeon Baek <u3582165@connect.hku.hk>
2025-04-02 16:32:37 +0800

    REST framework Added and Serializer Added

--- Accommodations/models.py
+++ Accommodations/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Accommodation(models.Model):
+    RENTAL_PERIOD_CHOICES = [
+        ('monthly', 'Monthly'),
+        ('weekly', 'Weekly'),
+        ('daily', 'Daily'),
+    ]
+
+    # Existing fields
+    accommodation_id = models.CharField(max_length=50, primary_key=True)
+    location = models.CharField(max_length=200)
+    rental_price = models.DecimalField(max_digits=10, decimal_places=2)
+    rental_period = models.CharField(max_length=7, choices=RENTAL_PERIOD_CHOICES, default='monthly')
+    number_of_beds = models.IntegerField()
+    number_of_bedrooms = models.IntegerField()
+    distance_from_campus = models.DecimalField(max_digits=10, decimal_places=2)
+    availability_status = models.BooleanField(default=True)
+    uploaded_date = models.DateTimeField(auto_add_now=True)
+    
+    
+    def __str__(self):
+        return f"{self.location} (${self.rental_price})"
--- UniHaven/settings.py
+++ UniHaven/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "Accommodations.apps.AccommodationsConfig"
+    "Accommodations",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
